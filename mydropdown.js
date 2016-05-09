fbafcaaaa1e780da85ef32a0160152448934b8fb
Lexiwu <sakura9641@gmail.com>
2016-05-09 21:25:01 +0800

    * my RWD 1. my dropdown list

---
+++ mydropdown.js
@@ -0,0 +1,31 @@
+﻿$(function () {
+
+
+
+
+
+
+
+    document.getElementById("menu-blog").onclick = function () {
+
+        document.getElementById("dropdown").classList.toggle("show");
+    };
+
+
+    // Close the dropdown if the user clicks outside of it
+    window.onclick = function (event) {
+        if (!event.target.matches('#menu-blog')) {
+
+            var dropdowns = document.getElementsByClassName("content");
+            var i;
+            for (i = 0; i < dropdowns.length; i++) {
+                var openDropdown = dropdowns[i];
+                if (openDropdown.classList.contains('show')) {
+                    openDropdown.classList.remove('show');
+                }
+            }
+        }
+    }
+
+
+});
